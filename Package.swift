7844a7bab78be8a3a882e58ba7a3c448a47f3336
Anumol Andrews <51256258+andrewsanumol@users.noreply.github.com>
2024-07-11 15:47:08 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,6 +8,9 @@ let package = Package(
     platforms: [
         .iOS(.v13)
     ],
+    platforms: [
+        .iOS("13.0")
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -18,7 +21,8 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SimpleToastSwiftUI"),
+            name: "SimpleToastSwiftUI",
+            path: "Sources"),
 
         .testTarget(
             name: "SimpleToastSwiftUITests",
