de66ac37dde2011dc79ba2c7571ce0d2d95e42ee
anumol <anumol@qburst.com>
2024-07-12 20:35:24 +0530

    update example app

---
+++ Example/TestApp/TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Anumol Andrews on 12/07/24.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
